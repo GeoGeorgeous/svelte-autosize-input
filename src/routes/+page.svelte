c408e6de77bb4558be45342f918ee8b882a93f6e
George Gorchev <g.gorchev@xbprime.com>
2024-06-06 21:01:44 +0000

    wip: demos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,27 @@
-<h1>Welcome to your library project</h1>
-<p>
-  Create your package using @sveltejs/package and preview/showcase your work
-  with SvelteKit
-</p>
+<script lang="ts">
+  import AutosizeInput from '@components/AutosizeInput.svelte';
+  let value1 = '';
+  let value2 = 'John Doe';
+  let value3 = '';
+  let value4 = '';
+</script>
+
+<h1>Svelte Autosize Input</h1>
+<p>An input component for Svelte that resizes itself to the current content.</p>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
+
+<div class="container">
+  <label for="1">Simple example with autofocus</label>
+  <AutosizeInput id="1" bind:value={value1} autofocus />
+</div>
+
+<div class="container">
+  <label for="name">Styled example with default value</label>
+  <AutosizeInput
+    bind:value={value2}
+    type="number"
+    placeholder="Your name"
+    style="padding: 0.25rem .5rem; border: none" />
+</div>
