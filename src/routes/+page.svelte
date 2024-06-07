522cccb96956901b3da457d0d79bb5c811c60ffe
George Gorchev <g.gorchev@xbprime.com>
2024-06-07 13:24:28 +0000

    demos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import AutosizeInput from '@components/AutosizeInput.svelte';
+  import { onMount } from 'svelte';
   let value1 = 'Type something to see the input grow...';
   let value2 = '14,200.24';
   let value3 = '';
@@ -7,6 +8,14 @@
   let value5 = '';
   let value6 = 'McDonalds';
   let value7 = 'Double Cheeseburger';
+
+  let nameRef;
+  let descriptionRef;
+  let containerRef;
+
+  onMount(() => {
+    console.log(nameRef);
+  });
 </script>
 
 <h1 class="mb-4 text-3xl">Svelte Autosize Input</h1>
@@ -65,7 +74,6 @@
   <AutosizeInput
     id="demo-input-5"
     bind:value={value5}
-    overflow
     placeholder="Type in something really long to see the input overflow the parent container"
     class="rounded border border-gray-300 bg-white px-2 py-1.5 font-medium text-gray-900 focus:border-blue-500 focus:ring-blue-500" />
 </div>
@@ -87,19 +95,21 @@
         d="M16 16h.01" /><path d="m2 16 20 6-6-20A20 20 0 0 0 2 16" /><path
         d="M5.71 17.11a17.04 17.04 0 0 1 11.4-11.4" /></svg>
   </div>
-  <div class="inline-flex flex-row items-center gap-1">
+  <div class="flex w-full max-w-full items-start gap-2">
     <AutosizeInput
+      bind:inputRef={nameRef}
       id="demo-input-5"
       bind:value={value6}
-      overflow
       placeholder="Name"
-      maxlength={20}
-      class="rounded bg-inherit px-2 py-2 font-semibold outline-none  transition-colors  hover:bg-gray-100 focus:bg-gray-100" />
+      placeholderIsMinWidth
+      class="rounded bg-inherit px-2 py-2 font-semibold outline-none transition-colors hover:bg-gray-100 focus:bg-gray-100" />
+
     <AutosizeInput
+      bind:inputRef={descriptionRef}
       id="demo-input-5"
       bind:value={value7}
-      overflow
-      placeholder="Name"
+      placeholder="Description"
+      placeholderIsMinWidth
       class="focus:bg-white-100 rounded bg-inherit px-2 py-[10px] text-sm font-semibold text-gray-600 outline-none  transition-colors hover:bg-gray-100 focus:bg-gray-100" />
   </div>
 </div>
