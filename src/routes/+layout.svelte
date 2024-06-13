4c7d3c6d2676413fedb3a5165e58d33244f8405e
George Gorchev <g.gorchev@xbprime.com>
2024-06-13 16:37:28 +0300

    Add Vercel analytics (#1)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
   import '@/app.css';
+  import { inject } from '@vercel/analytics';
+  import { dev } from '$app/environment';
+
+  inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <main class="mx-auto max-w-screen-lg px-6 py-12 lg:px-24">
